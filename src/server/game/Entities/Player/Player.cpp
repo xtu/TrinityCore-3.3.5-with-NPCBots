77d0fae454af0f0557e460a9c8a6b78502ac36ae
trickerer <onlysuffering@gmail.com>
2023-04-25 20:28:12 +0700

    NPCBots: Fix bots not being able to find next wander node after death in BG. Fix ownded bots being added to BG group twice.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -24456,7 +24456,8 @@ void Player::SetBattlegroundOrBattlefieldRaid(Group* group, int8 subgroup)
             if (!bot || !GetGroup()->IsMember(bot->GetGUID()))
                 continue;
 
-            ASSERT(group->AddMember(bot));
+            if (!group->IsMember(itr->first))
+                group->AddMember(bot);
         }
     }
     //end npcbot
