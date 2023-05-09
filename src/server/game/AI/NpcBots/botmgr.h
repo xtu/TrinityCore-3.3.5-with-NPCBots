8ee72c0d0696b5c7bf020339311a4bd1667aa2d6
trickerer <onlysuffering@gmail.com>
2023-05-09 09:05:58 +0700

    NPCBots: New config: dps modifiers by level bracket. Switch bot client-side ownership type back to pet (using owner guid). Add missing check for ranged dps target in IsInBotParty()

--- src/server/game/AI/NpcBots/botmgr.h
+++ src/server/game/AI/NpcBots/botmgr.h
@@ -126,6 +126,7 @@ class TC_GAME_API BotMgr
         static float GetBotWandererHPMod();
         static float GetBotWandererSpeedMod();
         static float GetBotDamageModByClass(uint8 botclass);
+        static float GetBotDamageModByLevel(uint8 botlevel);
 
         static void Initialize();
         static void ReloadConfig();
