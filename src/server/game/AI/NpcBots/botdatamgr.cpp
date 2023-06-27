5eea446842804fa7b267aa790713b836b182375d
trickerer <onlysuffering@gmail.com>
2023-06-27 08:38:48 +0700

    NPCBots: Fix a crash in `GetNextBGTravelNode()`. Fix a crash in `_LocalizeItem()`. Fix resilience gear requirement being applied to bots below level 80, causing BG bots to spawn with no gear. Fix chase movement angle. Fix Warrior BG bots generating caster shields.

--- src/server/game/AI/NpcBots/botdatamgr.cpp
+++ src/server/game/AI/NpcBots/botdatamgr.cpp
@@ -1662,7 +1662,7 @@ void BotDataMgr::CreateWanderingBotsSortedGear()
                         if (!is_caster_item)
                             push_gear_to_classes(proto, BOT_SLOT_OFFHAND, reqLstep, { BOT_CLASS_WARRIOR });
                         if (is_caster_item || proto.RequiredLevel < 60 || (proto.RequiredLevel < 69 && (proto.RandomProperty || proto.RandomSuffix)))
-                            push_gear_to_classes(proto, BOT_SLOT_OFFHAND, reqLstep, { BOT_CLASS_WARRIOR, BOT_CLASS_PALADIN, BOT_CLASS_SHAMAN, BOT_CLASS_SPELLBREAKER });
+                            push_gear_to_classes(proto, BOT_SLOT_OFFHAND, reqLstep, { BOT_CLASS_PALADIN, BOT_CLASS_SHAMAN, BOT_CLASS_SPELLBREAKER });
                         break;
                     case INVTYPE_HEAD:
                     case INVTYPE_SHOULDERS:
@@ -1973,7 +1973,7 @@ Item* BotDataMgr::GenerateWanderingBotItem(uint8 slot, uint8 botclass, uint8 lev
     if (!itemIdVec->empty())
     {
         ItemIdVector validVec;
-        validVec.reserve(itemIdVec->size() / 4);
+        validVec.reserve(itemIdVec->size());
         for (uint32 iid : *itemIdVec)
         {
             ItemTemplate const* proto = sObjectMgr->GetItemTemplate(iid);
