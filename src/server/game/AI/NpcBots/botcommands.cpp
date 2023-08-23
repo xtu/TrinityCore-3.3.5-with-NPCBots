952ebcffa119cd79b125d7bdb3cc7070583fe148
trickerer <onlysuffering@gmail.com>
2023-08-23 09:02:44 +0700

    NPCBots: Fix formatting errors 2

--- src/server/game/AI/NpcBots/botcommands.cpp
+++ src/server/game/AI/NpcBots/botcommands.cpp
@@ -721,7 +721,7 @@ public:
         });
         std::string val_str = ss.str();
         val_str.resize(val_str.size() - 1u);
-        trans->Append(val_str);
+        trans->PAppend("%s", val_str.c_str());
         WorldDatabase.CommitTransaction(trans);
 
         handler->SendSysMessage("Saved.");
